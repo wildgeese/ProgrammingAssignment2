d403605830dab6d6f801c305e0d4b9e39078b1cb
binyan <byan@utexas.edu>
2016-07-12 15:46:05 -0400

    edited the contents of the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(dim(x)[1]!=dim(x)[2]){
+    message("this is not a square matrix")
+    return()}
+  m<-matrix(nrow=dim(x)[1],ncol=dim(x)[1])
+  set<-function(y){
+    x<<-y
+    if(dim(x)[1]!=dim(x)[2]){
+      message("this is not a square matrix")
+      return()}
+    m<<-matrix(nrow=dim(x)[1],ncol=dim(x)[1])
+  }
+  get<-function(){x}
+  getinverse<-function(){m}
+  setinverse<-function(data){m<<-data}
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(length(m[!is.na(m)])>0){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setinverse(m)
+  m
 }
